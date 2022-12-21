82c3a60c3872dbdf9be2d6e495f14f6f3d3ab540
Daniel <dgplays14@gmail.com>
2022-12-21 20:07:37 -0300

    feat: add base

---
+++ src/styles/global.ts
@@ -0,0 +1,46 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const GlobalStyles = createGlobalStyle`
+    *,
+    *::before,
+    *::after {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+		font-size: 62.5%;
+    }
+
+    body {
+        min-height: 100vh;
+        width: 100%;
+    }
+
+    img {
+        width: 100%;
+    }
+
+    button {
+        border: 0;
+        background-color: transparent;
+        cursor: pointer;
+    }
+
+    li {
+        list-style: none;
+    }
+
+	h1,h2,h3,h4,h5,h6 {
+		font-weight: normal;
+		font-style: normal;
+	}
+
+    a {
+        text-decoration: none;
+    }
+
+    input {
+        border: 0;
+        outline: 0;
+        background-color: transparent;
+    }
+`
